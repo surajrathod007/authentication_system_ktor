257ac7dca8e585c847b5685e00f0b3be9c4c1351
Manshal <manshalkhatri51@gmail.com>
2022-06-18 18:05:40 +0530

    Added Auth & JWT Module

---
+++ src/auth/JWTService.kt
@@ -0,0 +1,24 @@
+package com.example.auth
+
+import com.auth0.jwt.JWT
+import com.auth0.jwt.JWTVerifier
+import com.auth0.jwt.algorithms.Algorithm
+import com.example.model.User
+import javax.xml.crypto.AlgorithmMethod
+
+class JWTService {
+    val issuer = "authServer"
+    val secret = "authorized"
+    val algorithm = Algorithm.HMAC512(secret)
+
+    val verifier : JWTVerifier = JWT.require(algorithm).withIssuer(issuer).build()
+
+
+    fun generateToken(user: User):String{
+        return JWT.create()
+            .withSubject("makeAuthorized")
+            .withIssuer(issuer)
+            .withClaim("emailId",user.emailId)
+            .sign(algorithm)
+    }
+}
